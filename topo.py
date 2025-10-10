d26cb5752e1ef3925b2018b985b5e56fb92a87a3
bryan <brygerkin@gmail.com>
2025-10-10 19:32:31 -0400

    Policy Template

--- topo.py
+++ topo.py
@@ -2,7 +2,7 @@
 """
 This is the most simple example to showcase Containernet.
 """
-import testyaml
+import yaml
 from mininet.net import Containernet
 from mininet.node import Controller
 from mininet.cli import CLI
@@ -13,9 +13,9 @@ setLogLevel('info')
 def hex_dpid(n: int) -> str:
     return f'{n:016x}'  # 16 hex digits, zero-padded
 
-def createPod(net, pod_idx, names, numHost):
+def createPod(net, name, pod_idx, names, numHost):
     info(f'*** Adding Process Switch {pod_idx}\n')
-    s = net.addSwitch(f's{pod_idx}',dpid=hex_dpid(pod_idx))
+    s = net.addSwitch(name,dpid=hex_dpid(pod_idx))
     hosts = []
     for host in range(numHost):
         i = host
@@ -27,11 +27,29 @@ def createPod(net, pod_idx, names, numHost):
         hosts.append(d)
     return s, hosts
 
+def createNetwork(net,name):
+    with open(name) as f:
+        network = yaml.safe_load(f)
+
+    for sw_name, devices in network["switches"].items():
+        s = net.addSwitch(sw_name)
+        for device in devices:
+            for name, IP in device.items():
+                d = net.addDocker(name, ip=IP, dimage="ubuntu:trusty")
+                net.addLink(d,s)
+
+    for sw_name, links in network["links"].items():
+        s = net.addSwitch(sw_name)
+        for link in links:
+            sw = net.get(link)
+            net.addLink(s,sw)
+
+
 def loadRules(net, name):
     with open(name) as f:
-        policy = testyaml.safe_load(f)
+        policy = yaml.safe_load(f)
 
-    for sw_name, commands in policy.items():
+    for sw_name, commands in policy['flows'].items():
         sw = net.get(sw_name)       # get Mininet switch object
         sw.cmd(f'ovs-ofctl del-flows {sw}')
         for command in commands:
@@ -41,13 +59,9 @@ def main():
     net = Containernet(controller=Controller)
     info('*** Adding controller\n')
     net.addController('c0')
-    s1 = net.addSwitch(f's1')
-    d1 = net.addDocker(f'd1', ip=f'10.0.1.1', dimage="ubuntu:trusty")
-    d2 = net.addDocker(f'd2', ip=f'10.0.1.2', dimage="ubuntu:trusty")
 
-    info('*** Creating switch links\n')
-    net.addLink(d1, s1)
-    net.addLink(d2, s1)
+    info('*** Creating Network\n')
+    createNetwork(net,"network.yaml")
 
     info('*** Starting network\n')
     net.start()
@@ -55,6 +69,7 @@ def main():
     info('*** Testing connectivity\n')
     net.pingAll()
 
+    info('*** Loading Rules\n')
     loadRules(net,"policy.yaml")
 
     info('*** Running CLI\n')
@@ -62,7 +77,6 @@ def main():
 
     info('*** Stopping network')
     net.stop()
-
 
 
 if __name__ == '__main__':
